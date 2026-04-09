d534c9fc59cd575cb3e8c873ca1f0704b28212bf
Kouta Dagnino <k.y.dagnino@gmail.com>
2026-04-09 10:56:58 +0200

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            row=[s for s in board[i] if s!="."]
+            col=[r[i] for r in board if r[i]!="."]
+            if len(set(row))!=len(row) or len(set(col))!=len(col):
+                return False
+        for i in range(3):
+            for j in range(3):
+                sub_board = [board[3*i+x//3][3*j+x%3] for x in range(9)]
+                square=[s for s in sub_board if s!="."]
+                if len(set(square))!=len(square):
+                    return False
+        return True
