ef6503ff0247dcb0868228d3d6929af339ecbfe3
Kouta Dagnino <k.y.dagnino@gmail.com>
2026-04-14 13:35:43 +0200

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        water_h = {i: 10**3 for i,h in enumerate(height)}
+
+        curr_h = 0
+        for i,h in enumerate(height):
+            if h > curr_h:
+                curr_h = h
+            water_h[i] = curr_h
+
+        curr_h = 0
+        result = 0
+        for i in range(len(height)):
+            h = height[-i-1]
+            if h > curr_h:
+                curr_h = h
+            water_h[len(height)-i-1] = min(curr_h, water_h[len(height)-i-1])
+            result += min(curr_h, water_h[len(height)-i-1]) - h
+        
+        return result
+        
+            
